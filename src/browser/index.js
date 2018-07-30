7ee0a087e1ac1ae9482ccd879c6594814ca72551
Doga Yuksel <dogayuksel@gmail.com>
2018-07-31 00:05:56 +0200

    refactor: remove unused browser code

--- src/browser/index.js
+++
@@ -1,8 +0,0 @@
-function component() {
-  const element = document.createElement('div');
-  const app = 'RB';
-  element.innerHTML = `Hello World, ${app}`;
-  return element;
-}
-
-document.body.appendChild(component());
